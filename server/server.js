67e32732851b9ebea9d5aba62b895c56689d1afa
Gil Kagan <gilkag2@gmail.com>
2023-01-16 14:32:17 +0200

    updated key

--- server/server.js
+++ server/server.js
@@ -16,21 +16,16 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', async (req, res) => {
-  res.send('hi');
-});
-
-
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 3000,
-      top_p: 0.1,
-      frequency_penalty: 0.5,
+      temperature: 0.6,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 5,
       presence_penalty: 1,
     });
     const botText = response.data.choices[ 0 ].text;
